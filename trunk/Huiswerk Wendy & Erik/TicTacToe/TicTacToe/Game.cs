37bdfdc1889ada607be66b6fc93f3d5576db1bc7
peters.w.erik <peters.w.erik@19446f38-10b9-11df-8bff-b18dc4c943ad>
2010-03-12 14:25:16 +0000

    Tic-Tac-Toe made by Wendy and Erik

---
+++ trunk/Huiswerk Wendy & Erik/TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public static bool GameRuns { get; set; }
+
+    }
+}

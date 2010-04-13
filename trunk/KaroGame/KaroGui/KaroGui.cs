4f64598221db09e621f73fce58244a7748cbe548
peters.w.erik <peters.w.erik@19446f38-10b9-11df-8bff-b18dc4c943ad>
2010-04-13 19:27:17 +0000

    Karo Gameproject with
    - C# Library project for the engine (as .dll used by other two projects)
    - C# WinForms project for the 'alpha' Gui
    - C# XNA 3.1 Windows Game for the 'final' Gui
    
    Next thursday I will have a USB stick or something with the newest DirectX SDK and the XNA 3.1 SDK so you can 'test' it a bit

---
+++ trunk/KaroGame/KaroGui/KaroGui.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KaroGui
+{
+    public partial class KaroGui : Form
+    {
+        public KaroGui()
+        {
+            InitializeComponent();
+        }
+    }
+}

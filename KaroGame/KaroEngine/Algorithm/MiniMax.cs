0c4639d50300abac6cc90b67053bb96a2575ba12
peters.w.erik <peters.w.erik@19446f38-10b9-11df-8bff-b18dc4c943ad>
2010-04-27 09:13:10 +0000

    comments

--- KaroGame/KaroEngine/Algorithm/MiniMax.cs
+++ KaroGame/KaroEngine/Algorithm/MiniMax.cs
@@ -26,31 +26,41 @@ namespace Karo
 
         private Board DoMiniMax(Board node, int depth, bool turnA)
         {
+            // if depth has reached zero, quit
             if (depth <= 0)
                 return node;
+
+            // if won, we also can quit
             if (node.IsWon())
                 return node;
 
+            // set alpha
             Board alpha = node;
             int alphaEvalution = Int32.MinValue;
 
+            // generate moves
             List<Board> possibleMoves = node.GenerateMoves(turnA);
 
+            // iterate trough moves
             foreach (Board b in possibleMoves)
             {
+                // switch turn if needed
                 bool nextTurn = true;
-
                 if(turnA)
                     nextTurn = false;
 
+                // calculate beta & evalution
                 Board beta = DoMiniMax(b, depth - 1, nextTurn);
                 int betaEvaluation = -1 * beta.Evaluation(nextTurn);
 
+                // check if alpha is smaller then beta
                 if (alphaEvalution < betaEvaluation)
                 {
+                    // alpha = beta
                     alphaEvalution = betaEvaluation;
                     alpha = b;
 
+                    // show debug information
                     if (Game.Instance.ShowDebug)
                         Logger.AddLine(depth + " Beta: " + betaEvaluation);
                 }
